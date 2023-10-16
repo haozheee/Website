ed0b6d8b7ff9263dddfc9a0f5af46f883b063232
Haozhe Zhou <haozhezh@cs.cmu.edu>
2023-10-16 00:57:39 -0400

    Updates

---
+++ precache-manifest.41a6ae7f4abee759bbd07069c36ebd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b970ec073fcf75ef0dde687bedc0e83a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9c22d3ca3214089e15",
+    "url": "/static/css/2.d22c8e4e.chunk.css"
+  },
+  {
+    "revision": "740ab849ded456a0ae3a",
+    "url": "/static/css/main.07c5a8e4.chunk.css"
+  },
+  {
+    "revision": "ae9c22d3ca3214089e15",
+    "url": "/static/js/2.450cfa5a.chunk.js"
+  },
+  {
+    "revision": "c3cced2dd16289785bff193db76a3198",
+    "url": "/static/js/2.450cfa5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740ab849ded456a0ae3a",
+    "url": "/static/js/main.c022f574.chunk.js"
+  },
+  {
+    "revision": "261e58cc322e8126f8a7",
+    "url": "/static/js/runtime-main.ba92f995.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Website/precache-manifest.57be980251d7c3224f71d2bf3bed97e1.js"
+  "/precache-manifest.41a6ae7f4abee759bbd07069c36ebd4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
